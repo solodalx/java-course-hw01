79c63abcb4101c52b8b787401ac01f6ccd86c2f5
solodalx <solodukha@mail.ru>
2017-09-24 00:10:53 +0300

    This is a first commit of homework number 1

---
+++ src/ru/sbt/javacourse/hw01/Main.java
@@ -0,0 +1,14 @@
+package ru.sbt.javacourse.hw01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main class");
+
+        System.out.println("Synced?");
+
+    }
+
+
+
+}
